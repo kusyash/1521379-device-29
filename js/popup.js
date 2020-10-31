2ce0d48b7de1a396bdf4bfc57ecf4ffcee4e6c03
Maria Zakharenkova <67202380+kusyash@users.noreply.github.com>
2020-10-31 22:14:54 +0300

    keyframes и стили формы + стили ссылок меню

--- js/popup.js
+++ js/popup.js
@@ -19,6 +19,7 @@ try {
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
+    popup.classList.remove("modal--error");
     popup.classList.add("modal--show");
     
     let hasFocus = false;
@@ -57,14 +58,43 @@ window.addEventListener("keydown", function (evt) {
     }    
 });
 
+name.addEventListener("focus", function (evt) {
+    name.classList.remove("field-invalid");
+});
+email.addEventListener("focus", function (evt) {
+    email.classList.remove("field-invalid");
+});
+message.addEventListener("focus", function (evt) {
+    message.classList.remove("field-invalid");
+});
+
 form.addEventListener("submit", function (evt) {
+    popup.classList.remove("modal--error");
+    popup.offsetWidth = popup.offsetWidth
+
     if (!name.value || !email.value || !message.value) {
         evt.preventDefault();
-        alert("Нужно заполнить все поля формы");
+
+        if (!name.value){
+            name.classList.add("field-invalid");
+        }
+
+        if (!email.value){
+            email.classList.add("field-invalid");
+        }
+
+        if (!message.value){
+            message.classList.add("field-invalid");
+        }
+
+        // alert("Нужно заполнить все поля формы");
+        console.log("Нужно заполнить все поля формы");
+        popup.classList.add("modal--error");
     } else {
         if(isStorageSupport) {
             localStorage.setItem("name", name.value);
             localStorage.setItem("email", email.value);
         }
     }
+
 });
