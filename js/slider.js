c325b5759bbb84612280b44c783aa8c4c881ccd2
Maria Zakharenkova <67202380+kusyash@users.noreply.github.com>
2020-11-03 20:04:25 +0300

    исправления на главной

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 const containerSlider = document.querySelector(".main-slider");
-const paginationSlider = containerSlider.querySelectorAll(".slider-pagination");
+const paginationSlider = containerSlider.querySelectorAll(".slider-pagination-item");
 const itemSlider = containerSlider.querySelectorAll(".slider-item");
 
 for(let index in paginationSlider){
@@ -10,7 +10,7 @@ for(let index in paginationSlider){
 
            for(let index in paginationSlider){
             if(paginationSlider.hasOwnProperty(index)){
-                paginationSlider[index].classList.remove("slider-pagination--active");
+                paginationSlider[index].classList.remove("slider-pagination-item--active");
             }
         }
 
@@ -19,7 +19,7 @@ for(let index in paginationSlider){
                     itemSlider[index].classList.remove("slider-item--active");
             }
         }
-            paginationSlider[index].classList.add("slider-pagination--active");
+            paginationSlider[index].classList.add("slider-pagination-item--active");
             itemSlider[index].classList.add("slider-item--active");
         });
     }
